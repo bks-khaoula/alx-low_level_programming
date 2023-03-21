4dad3e9a5b927a2bb16a85aac4fcf35614cf8831
khaoula <bakasskhaoula9@gmail.com>
2023-03-21 16:12:48 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *sh = "main";
+	/**
+	 * main - print _putchar
+	 * Return: 0
+	 */
 
-	while (*sh)
+
+	int main(void)
 	{
-		_putchar(*sh);
-		sh++;
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
 	return (0);
-}
+	}
