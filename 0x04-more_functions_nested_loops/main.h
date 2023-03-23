aa739c63e43a23270e5250c244d5392d1aa11f8a
khaoula <bakasskhaoula9@gmail.com>
2023-03-23 16:45:39 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,9 +3,7 @@
 
 /*
  * File: main.h
- * Auth: Brennan D Baraban
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
+ *       
  */
 
 int _putchar(char c);
