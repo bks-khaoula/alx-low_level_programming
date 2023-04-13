8b3d493a53f37d973c542aa6165ebe0fcf95377a
khaoula <bakasskhaoula9@gmail.com>
2023-04-13 16:45:28 -0700

    a function that concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,7 +14,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i = 0, j = 0, length1 = 0, length2 = 0;
 
 	while (s1 && s1[length1])
-		len1++;
+		length1++;
 	while (s2 && s2[length2])
 		length2++;
 
